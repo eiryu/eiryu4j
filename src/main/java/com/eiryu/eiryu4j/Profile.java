b06646d5c44a1ae54b62c65136b01c981580f41b
eiryu <eiryu.co@gmail.com>
2014-03-01 21:36:30 +0900

    initial commit

---
+++ src/main/java/com/eiryu/eiryu4j/Profile.java
@@ -0,0 +1,14 @@
+package com.eiryu.eiryu4j;
+
+/**
+ * My profile
+ * 
+ * @author eiryu
+ * 
+ */
+public class Profile {
+
+	public static final String NAME = "eiryu";
+	public static final String MESSAGE = "hello";
+
+}
